a3cb42131ad5cab24f369b4dee3c70ccfa9ff246
seete001 <sepehr.etmd@gmail.com>
2025-04-23 15:55:37 +0200

    dev commit message

--- csvProject.py
+++ csvProject.py
@@ -3,7 +3,7 @@ import csv
 with open('test.csv','r') as csvfile:
     reader = csv.reader(csvfile)
     total = 0
-    processed_date = []
+    processedd_date = []
     next(reader)
     for row in reader:
         name = row[0]
@@ -11,11 +11,11 @@ with open('test.csv','r') as csvfile:
         quantity = int(row[2])
         total += price * quantity
         #print(f"{name} : {total}")
-        processed_date.append([name, quantity, total]) 
+        processedd_date.append([name, quantity, total]) 
 
     with open('processed_data.csv', 'w', newline='') as outfile:
         writer = csv.writer(outfile)
         writer.writerow(['Name', 'Quantity', 'Total'])
-        writer.writerows(processed_date)
+        writer.writerows(processedd_date)
     print("Processed data has been written to processed_data.csv")
     
