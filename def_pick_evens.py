6720a91e5e8707a0c1cc0740e669d6ecee0a8e04
Sepehr <sepehr.etmd@gmail.com>
2025-07-17 18:46:45 +0200

    Delete def_pick_evens.py

--- def_pick_evens.py
+++
@@ -1,6 +0,0 @@
-def pick_evens(*args):
-    return [arg for arg in args if arg % 2 == 0]
-
-user_input = input("Enter numbers separated by spaces: ")
-numbers = [int(num) for num in user_input.split()]
-print(pick_evens(*numbers))
